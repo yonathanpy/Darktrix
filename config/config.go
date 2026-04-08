fcb01142be72da53394a84cf6944585e0acc599b
Yonathan      aka (witwizard,orlserg) <109464957+yonathanpy@users.noreply.github.com>
2026-04-08 12:05:05 +0000

    Create config.go

---
+++ config/config.go
@@ -0,0 +1,18 @@
+package config
+
+import (
+	"os"
+	"strconv"
+)
+
+type Config struct {
+	Port string
+}
+
+func Load() Config {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	return Config{Port: port}
+}
